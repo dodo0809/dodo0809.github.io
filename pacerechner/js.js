28e9ddab5d7208079e0e43e3d9323f86d46bcff1
dododododofj <101260587+dododododofj@users.noreply.github.com>
2022-10-12 18:07:22 +0200

    changes

--- pacerechner/js.js
+++ pacerechner/js.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     const Berechne = document.querySelector(".Berechne");
     Berechne.addEventListener("click", () => {
-    const a = document.querySelector(".Zeit").value;
-    const b = document.querySelector(".Strecke").value;
-    alert("Sie müssen " + b/a +" minuten pro kilometer laufen.")
-    
+        const a = document.querySelector(".Zeit").value;
+        const b = document.querySelector(".Strecke").value;
+        const ergebnis = a / b;
+        const c = document.querySelector("#ergebnis");
+        
+        if (isNaN(ergebnis)){
+            c.innerHTML= "not a number"
+        }
+        else{
+            c.innerHTML = ergebnis;
+        }
+
+      
+
     })
 });
-
