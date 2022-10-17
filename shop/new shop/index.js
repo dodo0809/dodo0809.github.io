4c1a0c86fb8dd0a8c04718ffd77989f41fdd19e5
dododododofj <101260587+dododododofj@users.noreply.github.com>
2022-10-17 12:07:57 +0200

    /einkaufen

---
+++ shop/new shop/index.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const a = document.querySelector(".bananen");
+    a.addEventListener("click", () => {
+        const b = document.querySelector(".bananen1").value;
+        const c = document.querySelector(".en");     
+        
+        c.innerHTML =  b + " bananen " 
+
+
+
+    })
+})
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const d = document.querySelector(".erdbeeren");
+    d.addEventListener("click", () => {
+        const e = document.querySelector(".erdbeeren1").value;
+        const f = document.querySelector(".bn");     
+        
+        f.innerHTML = e + "erdbeeren " 
+
+
+
+    })
+})
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const g = document.querySelector(".kartoffeln");
+    g.addEventListener("click", () => {
+        const h = document.querySelector(".kartoffeln1").value;
+        const i = document.querySelector(".kn");     
+        
+        i.innerHTML = h + " kartoffeln " 
+
+
+
+    })
+})
+
+
