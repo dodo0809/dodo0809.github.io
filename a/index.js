2cd826b46def18ebfed9379e6184c8df0a089c52
dododododofj <101260587+dododododofj@users.noreply.github.com>
2022-10-20 11:43:37 +0200

    a

---
+++ a/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const b = document.querySelector(".erdbeeren");
+    b.addEventListener("click", () => {
+        const c = document.querySelector(".erbeeren1").value;
+        const d = document.querySelector(".a");     
+        
+        d.innerHTML =  c + " erdbeeren " 
+
+
+
+    })
+})
